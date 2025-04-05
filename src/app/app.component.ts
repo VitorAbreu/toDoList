1e3393a888cf084155eb1ade1cbe9397d3149cf9
Vitor <vitoryanfma@gmail.com>
2025-04-05 17:50:37 -0300

    feat(toDoList): first structure, asset alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet /> <p>teste</p>',
+  template: '<router-outlet />',
 })
 export class AppComponent {}
