486655329cdd8ea868124b81919211e2a111ede3
Vitor <vitoryanfma@gmail.com>
2025-04-09 20:06:39 -0300

    feat(toDoList): using inputs and output to save items on list

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../../interfaces/IListItems.interface';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  addItem = signal(true);
+  #setListItems = signal<IListItems[]>(this.#parseItems());
+  getListItems = this.#setListItems.asReadonly();
 
+  #parseItems() {
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
+
+  getInputAndAddItem(value: IListItems) {
+    localStorage.setItem(
+      '@my-list', JSON.stringify([...this.#setListItems(),value])
+    );
+
+    return this.#setListItems.set(this.#parseItems());
+  }
 }
